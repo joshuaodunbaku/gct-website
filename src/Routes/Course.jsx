5b646c5cc51b62652b7aab0a372f726e0228f0a3
joshuaodunbaku <joshuaodunbaku@gmail.com>
2023-02-25 02:14:49 +0100

    forgot to clear all warnings, fixed one

--- src/Routes/Course.jsx
+++ src/Routes/Course.jsx
@@ -1,4 +1,4 @@
-import { Link, useLocation, useParams } from "react-router-dom";
+import { useLocation, useParams } from "react-router-dom";
 import BreadCrumb from "../Components/BreadCrumb/BreadCrumb";
 import SlideButton from "../Components/SlideButton/SlideButton";
 import { data } from "../data";
